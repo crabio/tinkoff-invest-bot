f44d59f8256ec29fae913bfda82f76643f3646be
Ivan <40871973+iakrevetkho@users.noreply.github.com>
2020-07-27 08:54:00 +0300

    Fix style for candles_loader main.py

--- candles_loader/main.py
+++ candles_loader/main.py
@@ -1,8 +1,6 @@
 import tinvest
 import pandas as pd
 
-TOKEN = "t.IodbD3G5wmc4_vOlZDLdTKe4N9VVpCoEIAmbTcFhqWVACCiXMTKCLYm0GP7v2SRf-ozJb5Vq6Ix67Bv5-ydMDA"
-
 client = tinvest.SyncClient(TOKEN, use_sandbox=True)
 api = tinvest.MarketApi(client)
 
@@ -10,9 +8,9 @@ api = tinvest.MarketApi(client)
 response = api.market_stocks_get()  # requests.Response
 if response.status_code == 200:
     # Parse
-    markets_instruments_list =  response.parse_json().payload
+    markets_instruments_list = response.parse_json().payload
     
-    markets_instruments_data = [ {
+    markets_instruments_data = [{
             'currency': markets_instrument.currency,
             'figi': markets_instrument.figi,
             'isin': markets_instrument.isin,
