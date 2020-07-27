00c3648e68e1700f982e727f9e7d997b58f80e61
Ivan <40871973+iakrevetkho@users.noreply.github.com>
2020-07-27 08:56:42 +0300

    Fix style for global rank parser

--- global_rank_parser/parse_global_rank.py
+++ global_rank_parser/parse_global_rank.py
@@ -10,14 +10,13 @@
 
 # %%
 import pandas as pd
-import json
 import requests
 import os
 
 
 # %%
 # Request settings
-params={
+params = {
     "limit": 2000
 }
 URL = "https://www.forbes.com/forbesapi/org/global2000/2020/position/true.json"
@@ -25,7 +24,7 @@ URL = "https://www.forbes.com/forbesapi/org/global2000/2020/position/true.json"
 
 # %%
 # Get data from Forbes website
-response = requests.get(URL, params = params)
+response = requests.get(URL, params=params)
 print(response.status_code)
 
 
@@ -41,7 +40,7 @@ organizations_json = original_json["organizationList"]["organizationsLists"]
 
 # Loop for filtering data about ecach organization
 for organization in organizations_json:
-    filtered_organization={}
+    filtered_organization = {}
     filtered_organization["Company"] = organization["organizationName"]
     filtered_organization["Country"] = organization["country"]
     filtered_organization["Industry"] = organization["industry"]
@@ -53,7 +52,6 @@ for organization in organizations_json:
     # Add filtered data to list
     list_of_organization_data.append(filtered_organization)
 
-
 # %%
 # Create DataFrame from list of oraganizations dictionaries
 df = pd.DataFrame(list_of_organization_data)
@@ -61,10 +59,9 @@ df = pd.DataFrame(list_of_organization_data)
 # Clear
 df.dropna(axis=1, how='all').dropna(axis=0, how='all')
 
-
 # %%
 # File save settings
-save_dir="../data"
+save_dir = "../data"
 file_name = "companies_rank.parquet"
 
 # Create "data" directory if it isn't exist
@@ -72,6 +69,4 @@ if not os.path.exists(save_dir):
     os.makedirs(save_dir)
     
 # Save to parquet
-df.to_parquet('{}/{}'.format(save_dir,file_name))
-
-
+df.to_parquet('{}/{}'.format(save_dir, file_name))
