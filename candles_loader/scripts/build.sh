b489c55a6b89bdae5118a8751d47ef13c058fc5f
Shchukin Ivan <shchukin@arrival.com>
2020-08-08 00:20:39 +0300

    Add code coverage

--- candles_loader/scripts/build.sh
+++ candles_loader/scripts/build.sh
@@ -4,4 +4,7 @@ echo "Build application with gom"
 go build -o build/main | exit 1
 
 echo "Run tests"
-go test -i | exit 1
+go test -v -coverpkg=./... -coverprofile=profile.cov ./...
+
+echo "Run test coverage"
+go tool cover -func profile.cov
