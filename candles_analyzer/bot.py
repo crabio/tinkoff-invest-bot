e88feec7d389ed2ecd6a4fa1f7aff5c58a98b80e
iakrevetkho <vanoschukin@yandex.ru>
2021-01-09 16:36:44 +0300

    Improve bot stop loss

--- candles_analyzer/bot.py
+++ candles_analyzer/bot.py
@@ -1,4 +1,5 @@
 import random
+import logging
 
 from enum import Enum
 
@@ -15,22 +16,22 @@ class MacdSignal(Enum):
 class TradingBot:
     def __init__(self, stop_loss=0.05):
         # RSI thresholds
-        self.rsi_oversold_threshold = 30
-        self.rsi_overbought_threshold = 70
+        self._rsi_oversold_threshold = 30
+        self._rsi_overbought_threshold = 70
 
         # Stop loss
-        self.stop_loss = stop_loss
+        self._stop_loss = stop_loss
         
         self.reset()
     
     def reset(self):
         # init flags
         # RSI
-        self.rsi_signal = RsiSignal.NaN
+        self._rsi_signal = RsiSignal.NaN
         # MACD
-        self.macd_prev_value = None
-        self.macd_prev_diff = None
-        self.macd_signal = MacdSignal.NaN
+        self._macd_prev_value = None
+        self._macd_prev_diff = None
+        self._macd_signal = MacdSignal.NaN
         
         # Bought price
         self.balance = 0
@@ -38,41 +39,41 @@ class TradingBot:
         self.profit = 0
         
         self.current_step = 0
-    
+
     def _set_rsi_signal(self, rsi_value):
         # RSI flag conditions
-        if rsi_value > self.rsi_overbought_threshold:
-            self.rsi_signal = RsiSignal.OverBought
-        elif rsi_value < self.rsi_oversold_threshold:
-            self.rsi_signal = RsiSignal.OverSold
+        if rsi_value > self._rsi_overbought_threshold:
+            self._rsi_signal = RsiSignal.OverBought
+        elif rsi_value < self._rsi_oversold_threshold:
+            self._rsi_signal = RsiSignal.OverSold
 
     def _set_macd_signal(self, macd_value):
         # Check that we have prev value for diff
-        if self.macd_prev_value is not None:
+        if self._macd_prev_value is not None:
             # Calc MACD diff
-            macd_diff = macd_value - self.macd_prev_value
+            macd_diff = macd_value - self._macd_prev_value
 
             # Check that we have prev diff for condition
-            if self.macd_prev_diff is not None:
+            if self._macd_prev_diff is not None:
                 # MACD flag conditions
                 # GoUp - \_/
                 # GoDown - /─\
-                if (self.macd_prev_diff <= 0) & (macd_diff > 0):
+                if (self._macd_prev_diff <= 0) & (macd_diff > 0):
                     # GoUp
-                    self.macd_signal = MacdSignal.GoUp
-                elif (self.macd_prev_diff >= 0) & (macd_diff < 0):
+                    self._macd_signal = MacdSignal.GoUp
+                elif (self._macd_prev_diff >= 0) & (macd_diff < 0):
                     # GoDown
-                    self.macd_signal = MacdSignal.GoDown
+                    self._macd_signal = MacdSignal.GoDown
 
             # Save current diff value as previous
-            self.macd_prev_diff = macd_diff
+            self._macd_prev_diff = macd_diff
 
         # Save current value as previous
-        self.macd_prev_value = macd_value
+        self._macd_prev_value = macd_value
 
 
     def _buy(self, current_price):
-        print("Step: %d Buy with price=%f" % (self.current_step, current_price))
+        logging.debug("Step: %d Buy with price=%f" % (self.current_step, current_price))
 
         # Set balance
         self.balance = current_price
@@ -86,24 +87,30 @@ class TradingBot:
         if self.balance != 0:
             self.profit += profit
             
-            print("Step: %d Sell with price=%f and profit %.2f%%" % (self.current_step, current_price, profit * 100))
-            print("Overall Profit %.2f%%" % (self.profit * 100))
+            logging.debug("Step: %d Sell with price=%f and profit %.2f%% overall %.2f%%" % \
+                (self.current_step, current_price, profit * 100, self.profit * 100))
             
             # Set balance
             self.balance = 0
         else:
-            print("ERROR. Can't sell, if we already sold.")
+            logging.error("Can't sell, if we already sold.")
         
 
     def _check_stop_loss(self, current_price):
         # Calc profit
         profit = (current_price - self.balance) / self.balance
 
-        if profit <= -self.stop_loss:
-            print("Step: %d Stop loss from %.2f to %.2f with profit %.2f%%" % (self.current_step, self.balance, current_price, profit * 100))
-
+        if profit <= -self._stop_loss:
             # Sell
-            self._sell(current_price)
+            # Set stop loss as profit
+            profit = -self._stop_loss
+            self.profit += profit
+
+            logging.debug("Step: %d Stop loss %.2f%% from %.2f to %.2f with profit %.2f%% overall %.2f%%" % \
+                (self.current_step, -self._stop_loss, self.balance, current_price, profit * 100, self.profit * 100))
+            
+            # Set balance
+            self.balance = 0
 
 
     def process(self, data):
@@ -118,13 +125,13 @@ class TradingBot:
 
         if self.balance == 0:
             # Try to find buy signal
-            if (self.rsi_signal == RsiSignal.OverSold) & (self.macd_signal == MacdSignal.GoUp):
+            if (self._rsi_signal == RsiSignal.OverSold) & (self._macd_signal == MacdSignal.GoUp):
                 # Buy
                 self._buy(current_price)
 
         else:
             # Try to find sell signal
-            if (self.rsi_signal == RsiSignal.OverBought) & (self.macd_signal == MacdSignal.GoDown):
+            if (self._rsi_signal == RsiSignal.OverBought) & (self._macd_signal == MacdSignal.GoDown):
                 # Sell
                 self._sell(current_price)
             else:
